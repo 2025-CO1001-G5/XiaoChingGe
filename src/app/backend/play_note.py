131a0e4dfcb76e8b82b64c64b18ea8581efad39e
Shawn Lin <spig100.roc@gmail.com>
2025-11-14 15:52:50 +0800

    bug: Fixed unexpect path resolving errors

--- src/app/backend/play_note.py
+++ src/app/backend/play_note.py
@@ -1,5 +1,13 @@
 from ctypes import *
-module = cdll.LoadLibrary("./_lib/play_sound.so")
+from importlib.resources import files
+from ctypes import cdll
 
-def play_sound(midi_note: int, duration: int):
-    module.play_sound(c_int(midi_not), c_long(duration))
+LIB_NAME = "play_note.so"
+
+def load_my_library():
+    lib_path = files('src.app.backend').joinpath('libs', LIB_NAME).resolve()
+    return cdll.LoadLibrary(lib_path)
+
+def play_note(midi_note: int, duration: int):
+    module = load_my_library()
+    module.playNote(c_int(midi_note), c_long(duration))
