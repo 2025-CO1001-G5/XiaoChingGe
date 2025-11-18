dc4174a80d259ff294c25981507ff73d520d429e
Shawn Lin <spig100.roc@gmail.com>
2025-11-18 16:32:24 +0800

    chore: Removed useless comment.

--- c_src/play_note.c
+++ c_src/play_note.c
@@ -6,21 +6,6 @@ double getFrequency(int midiNote);
 
 void playNote(int midiNote, int duration);
 
-//int main() {
-//
-//    int notes[] = {
-//        48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59,  // C3 ~ B3
-//        60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71,  // C4 ~ B4
-//        72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83,  // C5 ~ B5
-//    };
-//    int length = sizeof(notes) / sizeof(notes[0]);
-//
-//    for (int i = 0; i < length; i++) {
-//        playNote(notes[i], 1000);
-//        Sleep(50);
-//    }                                                    //上面是我用來測試的
-//    return 0;
-//}
 double getFrequency(int midiNote) {
     return 440.0 * pow(2.0, (midiNote - 69) / 12.0);
 }
